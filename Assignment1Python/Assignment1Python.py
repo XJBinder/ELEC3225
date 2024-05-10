de322c495f4c5ed0f17d443741fdeaf08163cdf7
xjbinder <XJBinder.117@gmail.com>
2024-05-09 21:58:55 -0400

    Classes, Methods, and UI Working for Python

--- Assignment1Python/Assignment1Python.py
+++ Assignment1Python/Assignment1Python.py
@@ -1,3 +1,5 @@
+# Tech with Tim's YouTube
+# https://www.youtube.com/watch?v=VchuKL44s6E
 # Corey Schafer's YouTube
 # https://www.youtube.com/watch?v=RSl87lqOXDE&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc&index=4
 
@@ -18,19 +20,19 @@ class User:
         self.wentworthID = wentworthID
 
     def show_all_info(self):
-        return 'Firstname:{} Lastname:{} Wentworth ID:{}'.format(self.first_name, self.last_name, self.wentworthID)
+        return 'Firstname:{}, Lastname:{}, Wentworth ID:{},'.format(self.first_name, self.last_name, self.wentworthID)
 
 
 # Creating Student Class
 class Student(User):
     def search_course(self):
-        print('Student Search Course Method')
+        print('\nStudent Search Course Method')
 
     def add_drop_course(self):
-        print('Student Add/Drop Course Method')
+        print('\nStudent Add/Drop Course Method')
 
     def show_schedule(self):
-        print('Student Print Schedule Method')
+        print('\nStudent Print Schedule Method')
 
 
 # Creating Instructor Class
@@ -44,13 +46,13 @@ class Instructor(User):
             self.wentworthID = 'N/A'
 
     def show_schedule(self):
-        print('Instructor Print Schedule Method')
+        print('\nInstructor Print Schedule Method')
 
     def show_class_list(self):
-        print('Instructor Print Class List Method')
+        print('\nInstructor Print Class List Method')
 
     def search_course(self):
-        print('Instructor Search Course Method')
+        print('\nInstructor Search Course Method')
 
 
 # Creating Admin Class
@@ -64,89 +66,179 @@ class Admin(User):
             self.wentworthID = 'N/A'
 
     def add_course(self):
-        print('Admin Add Course Method')
+        print('\nAdmin Add Course Method')
 
     def remove_course(self):
-        print('Admin Remove Course Method')
+        print('\nAdmin Remove Course Method')
 
     def add_user(self):
-        print('Admin Add User Method')
+        print('\nAdmin Add User Method')
 
     def remove_user(self):
-        print('Admin Remove User Method')
+        print('\nAdmin Remove User Method')
 
     def add_student(self):
-        print('Admin Add Student Method')
+        print('\nAdmin Add Student Method') # To Course
 
     def remove_student(self):
-        print('Admin Remove Student Method')
+        print('\nAdmin Remove Student Method') # From Course
 
     def search_roster(self):
-        print('Admin Search Roster Method')
+        print('\nAdmin Search Roster Method')
 
     def show_roster(self):
-        print('Admin Print Roster Method')
+        print('\nAdmin Print Roster Method')
 
     def search_course(self):
-        print('Admin Search Course Method')
+        print('\nAdmin Search Course Method')
 
     def show_course(self):
-        print('Admin Print Course Method')
+        print('\nAdmin Print Course Method')
 
 
 # Creating Objects
 user_object = User('Jon', 'Binder', 'W00409648')
 student_object = Student('Carson', 'Mershon', 'W00414141')
-professor_object = Instructor('Douglas', 'Dr. Dow')
+instructor_object = Instructor('Douglas', 'Dr. Dow')
 admin_object = Admin('Admin', 'Person')
 
-# Testing Methods
-print('')
+# Welcome Message
+print('\n\t  --Welcome to Leopard Web Class Registration--')
+print('This is a test to show methods and classes work in Python')
+print('\n\t--Test Objects--\n1) User\n2) Student\n3) Instructor\n4) Admin\n5) Exit')
+# Get User Input
+user_input = input('>> ')
 
-# User Class Test
-print(user_object.show_all_info())
-user_object.set_first_name('JonTest')
-user_object.set_last_name('BinderTest')
-user_object.set_wentworthID('W00409648Test')
-print(user_object.show_all_info())
+while user_input != '5':
+    # User Selected User
+    if user_input == '1':
+        print('\nUser:', user_object.show_all_info(), 'was selected.\n')
+        # Menu Items
+        print('1) Change First Name\n2) Change Last Name\n3) Change Wentworth ID')
+        user_input = input('>> ')
+        # Changing Users First Name
+        if user_input == '1':
+            new_attribute = input('\nChanging First Name\n>> ')
+            user_object.set_first_name(new_attribute)
+        # Changing Users Last Name
+        elif user_input == '2':
+            new_attribute = input('\nChanging Last Name\n>> ')
+            user_object.set_last_name(new_attribute)
+        # Changing Users Wentworth ID
+        elif user_input == '3':
+            new_attribute = input('\nChanging Wentworth ID\n>> ')
+            user_object.set_wentworthID(new_attribute)
+        else:
+            print('Invalid Input')
+    # User Selected Student
+    elif user_input == '2':
+        print('\nStudent:', student_object.show_all_info(), 'was selected.\n')
+        # Menu Items
+        print('1) Change First Name\n2) Change Last Name\n3) Change Wentworth ID')
+        print('4) Search Course\n5) Add/Drop Course\n6) Print Schedule')
+        user_input = input('>> ')
+        # Changing Students First Name
+        if user_input == '1':
+            new_attribute = input('\nChanging First Name\n>> ')
+            student_object.set_first_name(new_attribute)
+        # Changing Students Last Name
+        elif user_input == '2':
+            new_attribute = input('\nChanging Last Name\n>> ')
+            student_object.set_last_name(new_attribute)
+        # Changing Students Wentworth ID
+        elif user_input == '3':
+            new_attribute = input('\nChanging Wentworth ID\n>> ')
+            student_object.set_wentworthID(new_attribute)
+        # Search for Course
+        elif user_input == '4':
+            student_object.search_course()
+        # Add/Drop Course
+        elif user_input == '5':
+            student_object.add_drop_course()
+        # Print Schedule
+        elif user_input == '6':
+            student_object.show_schedule()
+        else:
+            print('Invalid Input')
+    # User Selected Instructor
+    elif user_input == '3':
+        print('\nInstructor:', instructor_object.show_all_info(), 'was selected.\n')
+        # Menu Items
+        print('1) Change First Name\n2) Change Last Name\n3) Search Course')
+        print('4) Print Class List\n5) Print Schedule')
+        user_input = input('>> ')
+        # Changing Instructors First Name
+        if user_input == '1':
+            new_attribute = input('\nChanging First Name\n>> ')
+            instructor_object.set_first_name(new_attribute)
+        # Changing Instructors Last Name
+        elif user_input == '2':
+            new_attribute = input('\nChanging Last Name\n>> ')
+            instructor_object.set_last_name(new_attribute)
+        # Search for Course
+        elif user_input == '3':
+            instructor_object.search_course()
+        # Print Class List
+        elif user_input == '4':
+            instructor_object.show_class_list()
+        # Print Schedule
+        elif user_input == '5':
+            instructor_object.show_schedule()
+        else:
+            print('Invalid Input')
+    # User Selected Admin
+    elif user_input == '4':
+        print('\nAdmin:', admin_object.show_all_info(), 'was selected.\n')
+        # Menu Items
+        print('1) Change First Name\n2) Change Last Name\n3) Add Course')
+        print('4) Remove Course\n5) Add User\n6) Remove User\n7) Add Student to Course')
+        print('8) Remove Student from Course\n9) Search Roster\n10) Print Roster')
+        print('11) Search Course\n12) Print Course')
+        user_input = input('>> ')
+        # Changing Admins First Name
+        if user_input == '1':
+            new_attribute = input('\nChanging First Name\n>> ')
+            admin_object.set_first_name(new_attribute)
+        # Changing Admins Last Name
+        elif user_input == '2':
+            new_attribute = input('\nChanging Last Name\n>> ')
+            admin_object.set_last_name(new_attribute)
+        # Add Course
+        elif user_input == '3':
+            admin_object.add_course()
+        # Remove Course
+        elif user_input == '4':
+            admin_object.remove_course()
+        # Add User
+        elif user_input == '5':
+            admin_object.add_user()
+        # Remove User
+        elif user_input == '6':
+            admin_object.remove_user()
+        # Add Student to Course
+        elif user_input == '7':
+            admin_object.add_student()
+        # Remove Student from Course
+        elif user_input == '8':
+            admin_object.remove_student()
+        # Search Roster
+        elif user_input == '9':
+            admin_object.search_roster()
+        # Print Roster
+        elif user_input == '10':
+            admin_object.show_roster()
+        # Search Course
+        elif user_input == '11':
+            admin_object.search_course()
+        # Print Course
+        elif user_input == '12':
+            admin_object.show_course()
+        else:
+            print('Invalid Input')
+    else:
+        print('Invalid Input')
 
-print('')
-
-# Student Class Test
-print(student_object.show_all_info())
-student_object.set_first_name('CarsonTest')
-student_object.set_last_name('MershonTest')
-student_object.set_wentworthID('W00414141Test')
-print(student_object.show_all_info())
-student_object.search_course()
-student_object.add_drop_course()
-student_object.show_schedule()
-
-print('')
-
-# Instructor Class Test
-print(professor_object.show_all_info())
-professor_object.set_first_name('DouglasTest')
-professor_object.set_last_name('Dr. DowTest')
-print(professor_object.show_all_info())
-professor_object.show_schedule()
-professor_object.show_class_list()
-professor_object.search_course()
-
-print('')
-
-# Admin Class Test
-print(admin_object.show_all_info())
-admin_object.set_first_name('AdminTest')
-admin_object.set_last_name('PersonTest')
-print(admin_object.show_all_info())
-admin_object.add_course()
-admin_object.remove_course()
-admin_object.add_user()
-admin_object.remove_user()
-admin_object.add_student()
-admin_object.remove_student()
-admin_object.search_roster()
-admin_object.show_roster()
-admin_object.search_course()
-admin_object.show_course()
+    # Menu Options
+    print('\n\t--Test Objects--\n1) User\n2) Student\n3) Instructor\n4) Admin\n5) Exit')
+    # Get User Input
+    user_input = input('>> ')
